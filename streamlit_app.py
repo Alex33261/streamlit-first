64d1fff6d2a1738f3a935b81d6f843e1ac3df622
Alexandre <50085299+Alex33261@users.noreply.github.com>
2023-08-03 17:15:36 +0200

    Update streamlit_app.py
    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ import streamlit as st
 st.title('To do list pour une reprise en douceur')
 
 st.subheader('Guillaume')
-st.write('This is some text.')
 st.markdown(st.subheader("- LLM "))
+st.write('Comme tu le vois, on a découvert streamlit. A mon avis c'est vraiment pas mal pour des petits formats d'applis et je vois parfaitement ce genre de format pour le projet LLM')
 st.markdown("- Item 2")
 st.markdown("- Item 3")
 
@@ -14,7 +14,7 @@ message.write("Hello Guillaume")
 
 st.divider()
 
-st.subheader('Activite Souliman')
+st.subheader('Souliman')
 st.text('This is some text.')
 
 st.divider()
