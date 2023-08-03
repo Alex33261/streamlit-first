bf8cd3ab8d24a33a4651d0b3092c35dd8cc2ca85
Alexandre <50085299+Alex33261@users.noreply.github.com>
2023-08-03 17:18:08 +0200

    Update streamlit_app.py
    commit des chage

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,20 +4,26 @@ import streamlit as st
 st.title('To do list pour une reprise en douceur')
 
 st.subheader('Guillaume')
-st.markdown(st.subheader("- LLM "))
-st.write('Comme tu le vois, on a découvert streamlit. A mon avis c est vraiment pas mal pour des petits formats d applis et je vois parfaitement ce genre de format pour le projet LLM')
-st.markdown("- Item 2")
-st.markdown("- Item 3")
-
 message = st.chat_message("assistant")
 message.write("Hello Guillaume")
+
+st.markdown(st.subheader("- LLM "))
+st.write('Comme tu le vois, on a découvert streamlit. A mon avis c est vraiment pas mal pour des petits formats d applis et je vois parfaitement ce genre de format pour le projet LLM')
+st.markdown("- Scentuarize")
+st.markdown("- Trois")
+
 
 st.divider()
 
 st.subheader('Souliman')
+message = st.chat_message("assistant")
+message.write("Hello Souliman, comment ca va ?")
 st.text('This is some text.')
 
 st.divider()
 
 st.subheader('Activite François')
+message = st.chat_message("assistant")
+message.write("Hello Souliman, comment ca va mon gâte?")
+
 st.text('This is some text.')
