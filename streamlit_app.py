ea5b6ff8a8c450fedd8e3277c427bd78b8249502
Alexandre <50085299+Alex33261@users.noreply.github.com>
2023-08-03 17:16:07 +0200

    Update streamlit_app.py
    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title('To do list pour une reprise en douceur')
 
 st.subheader('Guillaume')
 st.markdown(st.subheader("- LLM "))
-st.write('Comme tu le vois, on a découvert streamlit. A mon avis c'est vraiment pas mal pour des petits formats d'applis et je vois parfaitement ce genre de format pour le projet LLM')
+st.write('Comme tu le vois, on a découvert streamlit. A mon avis c est vraiment pas mal pour des petits formats d applis et je vois parfaitement ce genre de format pour le projet LLM')
 st.markdown("- Item 2")
 st.markdown("- Item 3")
 
