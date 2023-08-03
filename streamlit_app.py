2f170e7635bcf8809c2947228484dc87630c503c
Alexandre <50085299+Alex33261@users.noreply.github.com>
2023-08-03 16:53:03 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,20 @@
 import streamlit as st
 
 st.title('Activite Guillaume')
+st.header('A header with _italics_ :blue[colors] and emojis :sunglasses:')
+st.subheader('A subheader with _italics_ :blue[colors] and emojis :sunglasses:')
+st.text('This is some text.')
+
+st.divider()
+
 st.title('Activite Souliman')
+st.header('A header with _italics_ :blue[colors] and emojis :sunglasses:')
+st.subheader('A subheader with _italics_ :blue[colors] and emojis :sunglasses:')
+st.text('This is some text.')
+
+st.divider()
+
 st.title('Activite François')
+st.header('A header with _italics_ :blue[colors] and emojis :sunglasses:')
+st.subheader('A subheader with _italics_ :blue[colors] and emojis :sunglasses:')
+st.text('This is some text.')
