394b7da9532eac12ee762c8a62c0825fb7afa43b
Alexandre <50085299+Alex33261@users.noreply.github.com>
2023-08-04 16:46:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.balloons()
 image = Image.open('GuillaumeSourireMagique.jpg')
 st.image(image, caption='Un BG se cache dans cette image. Pouvez-vous le retrouver ?')
 
-st.title('        C est la reprise !')
+st.title('C est la reprise !')
 
 message = st.chat_message("assistant")
 message.write("Note importante : A compter de votre retour, tous vos projets doivent être dans Orchestra. Pensez donc à bien mettre à jour tous vos projets dans le logiciel. Le lien est le suivant https://mane.orchestra-ppm.cloud/cpms/")
@@ -21,7 +21,9 @@ st.write('Comme tu le vois, on a découvert streamlit. A mon avis c est vraiment
 st.write('Tu trouveras beaucoup d infos très intéressantes pour le projet Arômes que nous allons construire à la rentrée, notamment la réunion du 22 aout. Ce sont des infos techniques qui pourraient t aider à te projeter dans quelque chose de concret')
 st.write('https://langchain-examples.streamlit.app/')
 st.write('Creuse autour de de ce lien et de tous les autres liens que tu pourras trouver autour de LLM. Pense deja aussi a quelques exemples tres concrets de ce a quoi pourrait ressembler le ManeInsightGPT. Nous soumettrons ces exemples lors de la reunion du 22.')
+st.write('Vas aussi voir les liens dans la partie de Souliman qui pourraient t interesser.')
 
+         
 st.markdown("- Scentuarize")
 st.write('Julien partira un mois plus tôt pour faire une année supplémentaire sur un troisieme cycle. Il partira debut septembre. Nous avons eu une reunion avec David et une autre avec Julien et il a reussi a coller les deux dashboards')
 st.write('Il a reussi a pas mal avancer et a gagner en temps dans le traitements des donnees. Tu peux te rapprocher de lui pour reprendre le fil.')
@@ -41,7 +43,7 @@ message.write("Souliman")
 st.markdown("- TechniScent")
 st.write('L objectif est d integrer le travail de Lenaig dans un nouvel onglet de TechniScent avant la reunion du 8 septembre. J aimerais que tu puisse suivre son travail. Voila ce qu elle a à faire')
 st.write('1 - Mettre en place une information sur l ecart-type de la prediction fait à partir des comparaisons reel/predit de son jeu de test')
-st.write('2 - Obtenir un jeu de validation (une centaine de formules de Yann Lenne - equipe de Laurent Bacoux) et faire tourner sur l appli directement. En sortir une valeur pratique de l ecart type')
+st.write('2 - Obtenir un jeu de validation (une centaine de formules de Yann Lenne - equipe de Laurent Bacoux) et faire tourner sur l appli directement. En sortir une valeur pratique de l ecart type.')
 st.write('3 - Ameliorer l UI de maniere simple')
 st.write('De maniere generale, laisse la progresse à son rythme et integre son travail à te dernière version de TechniScent quand elle a fini. Pense aussi à demander à François le nouveau logo et une petite palette de couleur pour mettre le nouvel UI de TechniScent en place.')
 
@@ -56,7 +58,15 @@ message = st.chat_message("user")
 message.write("François")
 
 st.markdown("- TechniScent")
-st.write("Texte ici")
+st.write("Mettre la nouvelle UI avec Souliman")
+
+st.markdown("- Projet avec La Plateforme")
+st.write("Caler un rendez-vous Deborah. J ai deux projets en tete pour elle incluant la propriete intellectuelle : un chatbot et un outil de resume de brevets.")
+
+st.markdown("- Projet avec Reality")
+st.write("Construire les differents univers et les peupler. On validera une une maquette finale avec les differents contenus quand je reviens.")
+
+
 
 
 
