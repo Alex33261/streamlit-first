4745f048e725ce5361ca5174b79f5f1f5ec5e95c
Alexandre <50085299+Alex33261@users.noreply.github.com>
2023-08-04 11:51:56 +0200

    Update streamlit_app.py
    ballons

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,13 @@ import streamlit as st
 st.title('To do list pour une reprise en douceur')
 
 st.subheader('Guillaume')
+st.caption('Balloons. Hundreds of them...')
 message = st.chat_message("assistant")
 message.write("Hello Guillaume, alors ces vacances ? Pas trop chaud ? Voilà le topo pour une reprise en douceur...")
 
 st.markdown(st.subheader("- Orchestra "))
 
-st.markdown(st.subheader("- LLM "))
+st.markdown("- LLM ")
 st.write('Comme tu le vois, on a découvert streamlit. A mon avis c est vraiment pas mal pour des petits formats d applis et je vois parfaitement ce genre de format pour le projet LLM. 
 Tu peux aussi deployer des applis avec HuggingFace en streamlit et c est tres rapide.')
 
