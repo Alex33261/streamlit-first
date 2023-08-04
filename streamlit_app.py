65e268e8cff324650be1a7b897bab49c188aa9c7
Alexandre <50085299+Alex33261@users.noreply.github.com>
2023-08-04 15:54:54 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,39 +11,32 @@ st.title('To-do list pour une reprise en douceur')
 message = st.chat_message("assistant")
 message.write("Note importante : A compter de votre retour, tous vos projets doivent être dans Orchestra. Pensez donc à bien mettre à jour tous vos projets dans le logiciel. Le lien est le suivant https://mane.orchestra-ppm.cloud/cpms/")
 message.write("A compte du 28 aout, nous ferons des reunions de suivi de projets de 30 mn par personne pour strucuter les (nombreux) projets que nous avons.")
-
-
 st.divider()
 
+message = st.chat_message("user")
+message.write("Guillaume")
 
-st.subheader('Guillaume')
-
-st.markdown(st.subheader("- LLM "))
+st.markdown("- LLM ")
 st.write('Comme tu le vois, on a découvert streamlit. A mon avis c est vraiment pas mal pour des petits formats d applis et je vois parfaitement ce genre de format pour le projet LLM. Tu peux aussi deployer des applis avec HuggingFace en streamlit et c est tres rapide.')
 
-st.markdown(st.subheader("- Scentuarize"))
-st.write('Julien partira un mois plus tôt pour faire une année supplémentaire.')
+st.markdown("- Scentuarize")
+st.write('Julien partira un mois plus tôt pour faire une année supplémentaire sur un troisieme cycle. Il partira debut septembre. Nous avons eu une reunion avec David et une autre avec Julien et il a reussi a coller les deux dashboards')
+st.write('Il a reussi a pas mal avancer et a gagner en temps dans le traitements des donnees. Tu peux te rapprocher de lui pour reprendre le fil.')
 
 st.markdown("- Trois")
 
-
 st.divider()
 
-st.subheader('Souliman')
-
-st.text('This is some text.')
+message = st.chat_message("user")
+message.write("Souliman")
 
 st.divider()
 
 st.subheader('Activite François')
-message = st.chat_message("assistant")
-message.write("Hello Souliman, comment ca va mon gâte?")
+message = st.chat_message("user")
+message.write("François")
 
-st.text('This is some text.')
+st.write("Texte ici")
 
-col1, col2, col3 = st.columns(3)
-col1.write('Column 1')
-col2.write('Column 2')
-col3.write('Column3')
 
 
