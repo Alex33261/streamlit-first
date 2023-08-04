0cd1fad46d3ca0a4af2bff92008b5cf6bb333178
Alexandre <50085299+Alex33261@users.noreply.github.com>
2023-08-04 15:45:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,20 @@
 import streamlit as st
 from PIL import Image
 
+st.ballons()
 
 image = Image.open('GuillaumeSourireMagique.jpg')
 st.image(image, caption='Un BG se cache dans cette image. Pouvez-vous le retrouver ?')
 
 st.title('To-do list pour une reprise en douceur')
 
-message = st.chat_message("user")
+message = st.chat_message("assistant")
 message.write("Note importante : A compter du XXX tous nos projets seront dans Orchestra. Pensez à bien mettre à jour tous vos projets dans le logiciel. Le lien est le suivant https://mane.orchestra-ppm.cloud/cpms/")
 
+st.divider()
+
+
 st.subheader('Guillaume')
-st.caption('Balloons. Hundreds of them...')
-
-
-st.markdown(st.subheader("- Orchestra "))
 
 st.markdown(st.subheader("- LLM "))
 st.write('Comme tu le vois, on a découvert streamlit. A mon avis c est vraiment pas mal pour des petits formats d applis et je vois parfaitement ce genre de format pour le projet LLM. Tu peux aussi deployer des applis avec HuggingFace en streamlit et c est tres rapide.')
